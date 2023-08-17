95924fbbe439abfcc2f7434e5200eeff6633098a
RussellAbraham <russell_a@live.com>
2023-08-16 19:21:10 -0700

    Add more data to the items seed file

--- db/seeds/03_items.sql
+++ db/seeds/03_items.sql
@@ -1,31 +1,47 @@
 -- Insert items for user 1
 INSERT INTO items (description, checked, user_id, category_id)
 VALUES
-    ('Watch movie "Inception"', FALSE, 1, 1),
-    ('Read book "The Great Gatsby"', FALSE, 1, 3),
-    ('Buy groceries', FALSE, 1, 4),
-    ('Visit new restaurant downtown', FALSE, 1, 2);
+    ('Watch movie "The Matrix"', FALSE, 1, 1),
+    ('Read book "Harry Potter and the Sorcerer''s Stone"', FALSE, 1, 3),
+    ('Buy fruits and vegetables', FALSE, 1, 4),
+    ('Try a new sushi restaurant', FALSE, 1, 2),
+    ('Visit a museum', FALSE, 1, 1),
+    ('Read book "Pride and Prejudice"', FALSE, 1, 3),
+    ('Purchase a new phone case', FALSE, 1, 4),
+    ('Have a picnic in the park', FALSE, 1, 2);
 
 -- Insert items for user 2
 INSERT INTO items (description, checked, user_id, category_id)
 VALUES
-    ('Watch TV series "Stranger Things"', FALSE, 2, 1),
-    ('Read book "Dune"', FALSE, 2, 3),
-    ('Buy gifts for family', FALSE, 2, 4),
-    ('Try cooking a new recipe', FALSE, 2, 2);
+    ('Watch TV series "Breaking Bad"', FALSE, 2, 1),
+    ('Read book "The Lord of the Rings"', FALSE, 2, 3),
+    ('Buy ingredients for a homemade pizza', FALSE, 2, 4),
+    ('Explore a new cafe', FALSE, 2, 2),
+    ('Try a new workout routine', FALSE, 2, 1),
+    ('Read book "To Kill a Mockingbird"', FALSE, 2, 3),
+    ('Purchase gardening tools', FALSE, 2, 4),
+    ('Go for a hiking trip', FALSE, 2, 2);
 
 -- Insert items for user 3
 INSERT INTO items (description, checked, user_id, category_id)
 VALUES
-    ('Explore local hiking trails', FALSE, 3, 1),
-    ('Read book "The Hobbit"', FALSE, 3, 3),
-    ('Order gardening supplies', FALSE, 3, 4),
-    ('Dine out at a fancy restaurant', FALSE, 3, 2);
+    ('Watch documentary "Planet Earth II"', FALSE, 3, 1),
+    ('Read book "The Catcher in the Rye"', FALSE, 3, 3),
+    ('Buy cleaning supplies', FALSE, 3, 4),
+    ('Try a new Italian restaurant', FALSE, 3, 2),
+    ('Visit a botanical garden', FALSE, 3, 1),
+    ('Read book "Fahrenheit 451"', FALSE, 3, 3),
+    ('Purchase art materials', FALSE, 3, 4),
+    ('Take a day trip to the beach', FALSE, 3, 2);
 
 -- Insert items for user 4
 INSERT INTO items (description, checked, user_id, category_id)
 VALUES
-    ('Watch documentary "Planet Earth"', FALSE, 4, 1),
-    ('Read book "1984"', FALSE, 4, 3),
-    ('Shop for home decor', FALSE, 4, 4),
-    ('Try a new coffee shop', FALSE, 4, 2);
+    ('Watch movie "Interstellar"', FALSE, 4, 1),
+    ('Read book "The Alchemist"', FALSE, 4, 3),
+    ('Buy a birthday gift', FALSE, 4, 4),
+    ('Explore a new sushi place', FALSE, 4, 2),
+    ('Go stargazing', FALSE, 4, 1),
+    ('Read book "The Hunger Games"', FALSE, 4, 3),
+    ('Purchase home organization supplies', FALSE, 4, 4),
+    ('Attend a live music event', FALSE, 4, 2);
