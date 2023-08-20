1401e9ff87a53defbfc57e8273e570ec2dc7a260
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-08-20 16:10:03 -0700

    create db query to get items for a user

--- db/queries/items.js
+++ db/queries/items.js
@@ -1,12 +1,17 @@
 const db = require("../connection");
 
-const getUserItems = () => {
-  let queryString =
-    "SELECT categories.name,description FROM items JOIN categories ON category_id=categories.id WHERE user_id = 3 GROUP BY categories.name,description;";
+const getUserItems = (id) => {
+  let queryString = `SELECT categories.name,description FROM items JOIN categories ON category_id=categories.id WHERE user_id = $1 GROUP BY categories.name,description`;
+  let options = [id];
 
-  return db.query(queryString).then((data) => {
-    return data.rows;
-  });
+  return db
+    .query(queryString, options)
+    .then((data) => {
+      return data.rows;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 };
 
 module.exports = { getUserItems };
