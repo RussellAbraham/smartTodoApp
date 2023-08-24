4a81343b8e84b6a73fddbd3ff3880cafee8f0898
TJ <teejaynam@gmail.com>
2023-08-24 14:42:52 -0700

    update POST function to retrieve item object from query and pass into createObject function

--- routes/helper/getCategory.js
+++ routes/helper/getCategory.js
@@ -1,17 +1,40 @@
-const request = require('request');
+const request = require('request-promise');
 
-const API_KEY ='AIzaSyDF2g-IEWqr1Wc31dhYikJsbwEw9QgbyKo';
+const createObject = require('./createObject');
 
-const QUERY = 'The Boys'
+const process = require('process'); //for testing
+const { response } = require('express');
+const { error } = require('console');
 
-const API_URL = `https://kgsearch.googleapis.com/v1/entities:search?query=${encodeURIComponent(QUERY)}&key=${API_KEY}&limit=1&indent=True`;
+const readKey = '8ysE02L4zpcx'; //read
+
+const QUERY = process.argv[2]; //for testing
 
 
-request(API_URL, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const data = JSON.parse(body);
-    console.log(JSON.stringify(data));
-  }
-});
+const classify = {
+  method: 'POST',
+  uri: 'https://api.uclassify.com/v1/tjdude/smart-classifier/classify', //or /keywords
+  headers: {
+    Authorization: `Token ${readKey}`,
+    'Content-Type': 'application/json',
+  },
+  json: true,
+  body: {
+    texts: [`"${QUERY}"`]
+    ,
+  },
+};
+
+request(classify)
+  .then((response) => {
+    console.log(createObject.createObject(QUERY,response));
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
+
+module.exports = { }
+
+  //create logic to categorize things or not categorize things
+  //keep training model
