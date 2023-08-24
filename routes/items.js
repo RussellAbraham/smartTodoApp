c2da04810c9aa7be56a8747f066f246e61003d9b
RussellAbraham <russell_a@live.com>
2023-08-23 18:51:00 -0700

    Update items template to include id for ul element

--- routes/items.js
+++ routes/items.js
@@ -22,6 +22,7 @@ router.get("/", (req, res) => {
       res.render("index", { loggedInUser, itemsList });
       //the template variable for the ejs goes here
       console.log(loggedInUser);
+      console.log(result);
     });
   }
 });
