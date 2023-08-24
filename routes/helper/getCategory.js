e70995d17c059c9ed8fe40e22eb92afd3b0b8e60
TJ <teejaynam@gmail.com>
2023-08-24 15:32:13 -0700

    update function to return item's category ID

--- routes/helper/getCategory.js
+++ routes/helper/getCategory.js
@@ -2,39 +2,37 @@ const request = require('request-promise');
 
 const createObject = require('./createObject');
 
-const process = require('process'); //for testing
+//const process = require('process'); //for testing
 const { response } = require('express');
 const { error } = require('console');
 
 const readKey = '8ysE02L4zpcx'; //read
 
-const QUERY = process.argv[2]; //for testing
+//const QUERY = process.argv[2]; //for testing
 
+const classifyItem = function(query) {
+  const classify = {
+    method: 'POST',
+    uri: 'https://api.uclassify.com/v1/tjdude/smart-classifier/classify', //or /keywords
+    headers: {
+      Authorization: `Token ${readKey}`,
+      'Content-Type': 'application/json',
+    },
+    json: true,
+    body: {
+      texts: [`${query}`]
+      ,
+    },
+  };
 
-const classify = {
-  method: 'POST',
-  uri: 'https://api.uclassify.com/v1/tjdude/smart-classifier/classify', //or /keywords
-  headers: {
-    Authorization: `Token ${readKey}`,
-    'Content-Type': 'application/json',
-  },
-  json: true,
-  body: {
-    texts: [`"${QUERY}"`]
-    ,
-  },
-};
+  request(classify)
+    .then((response) => {
+      const item = createObject.createObject(query,response);
+      return item.categoryID;
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
 
-request(classify)
-  .then((response) => {
-    console.log(createObject.createObject(QUERY,response));
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
-
-
-module.exports = { }
-
-  //create logic to categorize things or not categorize things
-  //keep training model
+module.exports = { classifyItem: classifyItem };
